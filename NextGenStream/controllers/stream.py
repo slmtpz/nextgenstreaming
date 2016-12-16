37f218c45cc73ff9a8bec515e2bc8fe63bbb5f47
A. Seleme Topuz <seleme94@hotmail.com>
2016-12-16 20:53:50 +0300

    html bridge is established

--- NextGenStream/controllers/stream.py
+++ NextGenStream/controllers/stream.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def get_stream(req):
-    return render(req, 'NextGenStream/')
+    return render(req, 'NextGenStream/home.html')
